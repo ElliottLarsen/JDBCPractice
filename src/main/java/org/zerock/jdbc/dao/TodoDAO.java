c732bc98aa5905116ad48525d94fa7b8e2a5d1af
Elliott Larsen <elliottlrsn@gmail.com>
2023-12-27 18:22:43 -0700

    Hikari setup.

---
+++ src/main/java/org/zerock/jdbc/dao/TodoDAO.java
@@ -0,0 +1,4 @@
+package org.zerock.jdbc.dao;
+
+public class TodoDAO {
+}
--- src/test/java/org/zerock/dao/ConnectionTests.java
+++ src/test/java/org/zerock/dao/ConnectionTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectionTests {
 
@@ -17,6 +19,25 @@ public class ConnectionTests {
         );
 
         Assertions.assertNotNull(connection);
+        connection.close();
+    }
+
+    @Test
+    public void testHikariCP() throws Exception {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.mariadb.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mariadb://localhost:3306/webdb");
+        config.setUsername("webuser");
+        config.setPassword("webuser");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        HikariDataSource ds = new HikariDataSource(config);
+        Connection connection = ds.getConnection();
+
+        System.out.println(connection);
+
         connection.close();
     }
 }
