be7a7d11d21f35c0710e8f8e0c348570b73f6e66
Elliott Larsen <elliottlrsn@gmail.com>
2023-12-28 18:53:29 -0700

    Todo CREATE added.

--- src/main/java/org/zerock/jdbc/dao/TodoDAO.java
+++ src/main/java/org/zerock/jdbc/dao/TodoDAO.java
@@ -1,6 +1,8 @@
 package org.zerock.jdbc.dao;
 
+import org.zerock.jdbc.domain.TodoVO;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import lombok.Cleanup;
@@ -16,5 +18,18 @@ public class TodoDAO {
         String now = resultSet.getString(1);
 
         return now;
+    }
+
+    public void insert(TodoVO vo) throws Exception {
+        String sqlQuery = "INSERT INTO tbl_todo (title, dueDate, finished) values (?, ?, ?)";
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+
+        preparedStatement.setString(1, vo.getTitle());
+        preparedStatement.setDate(2, Date.valueOf(vo.getDueDate()));
+        preparedStatement.setBoolean(3, vo.isFinished());
+
+        preparedStatement.executeUpdate();
+
     }
 }
--- src/test/java/org/zerock/dao/TodoDAOTests.java
+++ src/test/java/org/zerock/dao/TodoDAOTests.java
@@ -3,6 +3,9 @@ package org.zerock.dao;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.zerock.jdbc.dao.TodoDAO;
+import org.zerock.jdbc.domain.TodoVO;
+
+import java.time.LocalDate;
 
 public class TodoDAOTests {
 
@@ -16,5 +19,15 @@ public class TodoDAOTests {
     @Test
     public void testTime() throws Exception {
         System.out.println(todoDAO.getTime());
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        TodoVO todoVO = TodoVO.builder()
+                .title("Sample Title...")
+                .dueDate(LocalDate.of(2023, 12, 28))
+                .build();
+
+        todoDAO.insert(todoVO);
     }
 }
