657ffc02884f36248e2f319472c1bbd1d6b06dc9
Elliott Larsen <elliottlrsn@gmail.com>
2023-12-29 19:03:37 -0700

    selectAll added.

--- src/main/java/org/zerock/jdbc/dao/TodoDAO.java
+++ src/main/java/org/zerock/jdbc/dao/TodoDAO.java
@@ -5,6 +5,9 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.Cleanup;
 
 public class TodoDAO {
@@ -31,5 +34,28 @@ public class TodoDAO {
 
         preparedStatement.executeUpdate();
 
+    }
+
+    public List<TodoVO> selectAll() throws Exception {
+        String sqlQuery = "SELECT * FROM tbl_todo";
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+        List<TodoVO> list = new ArrayList<>();
+
+        while (resultSet.next()) {
+            TodoVO vo = TodoVO.builder()
+                    .tno(resultSet.getLong("tno"))
+                    .title(resultSet.getString("title"))
+                    .dueDate(resultSet.getDate("dueDate").toLocalDate())
+                    .finished(resultSet.getBoolean("finished"))
+                    .build();
+
+            list.add(vo);
+        }
+
+        return list;
     }
 }
