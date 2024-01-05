00b5f5627570706f7fe84e7b90dfb4832f05f894
Elliott Larsen <elliottlrsn@gmail.com>
2024-01-04 20:44:04 -0700

    MemberVO and MemberDAO added.

---
+++ src/main/java/org/zerock/jdbc/dao/MemberDAO.java
@@ -0,0 +1,34 @@
+package org.zerock.jdbc.dao;
+
+import lombok.Cleanup;
+import org.zerock.jdbc.domain.MemberVO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MemberDAO {
+    public MemberVO getWithPassword(String mid, String mpw) throws Exception {
+        String query = "SELECT mid, mpw, mname from tbl_mbmer where mid = ? and pwd = ?";
+
+        MemberVO memberVO = null;
+
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+        preparedStatement.setString(1, mid);
+        preparedStatement.setString(2, mpw);
+
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        memberVO = MemberVO.builder()
+                .mid(resultSet.getString(1))
+                .mpw(resultSet.getString(2))
+                .mname(resultSet.getString(3))
+                .build();
+
+        return memberVO;
+    }
+}
